a2a6a45717a41c8eed5fe2f661ce2a67dc509dc0
vikasverma-here <vikasverma995619@gmail.com>
2024-11-15 20:24:49 +0530

    react first class-done

---
+++ class-one/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-[80%] mx-auto rounded mt-10 bg-red-200  p-10'>
+     <h1> 1 : Learn how to setup first react app </h1> 
+     <h1> 2 : Vertual dom </h1>
+     <h1> 3 : Reconcialiation  </h1>
+     <h1> 4 : how to install talwind in react app </h1>
+    </div>
+  )
+}
+
+export default App
